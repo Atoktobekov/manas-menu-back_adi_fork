e41ca4b5ac1d8191d172b4555196af31dfa45283
Adi <atoktobekov1@gmail.com>
2026-02-20 14:33:54 +0600

    fix json files saving

--- public/services/saveJsonFiles.js
+++ public/services/saveJsonFiles.js
@@ -1,14 +1,24 @@
 import fs from 'fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
 import { fetchAndParseMenu } from '../parsers/menuParser.js';
 import { fetchAndParseKiraathane } from '../parsers/kiraathaneParser.js';
+
+// Получаем директорию текущего файла
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function saveJsonFiles() {
     const menuData = await fetchAndParseMenu();
     const kiraathaneData = await fetchAndParseKiraathane();
 
-    await fs.writeFile('menu.json', JSON.stringify(menuData, null, 2));
-    await fs.writeFile('buffet.json', JSON.stringify(kiraathaneData, null, 2));
+    const menuPath = path.join(__dirname, "../menu.json");
+    const buffetPath = path.join(__dirname, "../buffet.json");
+
+
+    await fs.writeFile(menuPath, JSON.stringify(menuData, null, 2));
+    await fs.writeFile(buffetPath, JSON.stringify(kiraathaneData, null, 2));
 
     console.log('Files updated successfully');
 }
